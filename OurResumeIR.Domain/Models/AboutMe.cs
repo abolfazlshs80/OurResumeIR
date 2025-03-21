054d0c7ae639aa6be355920ef7922f9500f8745c
Mohammad Faraji <m.faraji9841@gmail.com>
2025-03-21 14:07:46 +0330

    Add Modeles

---
+++ OurResumeIR.Domain/Models/AboutMe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurResumeIR.Domain.Models
+{
+    public class AboutMe
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int UserId { get; set; }
+
+        // Navigation Property
+        public User User { get; set; }
+
+    }
+}
---
+++ OurResumeIR.Domain/Models/Blog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurResumeIR.Domain.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Text { get; set; }
+        public int UserId { get; set; }
+
+        // Navigation Property  
+        public User User { get; set; }
+    }
+}
