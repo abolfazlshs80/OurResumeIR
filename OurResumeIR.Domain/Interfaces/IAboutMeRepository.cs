3684578c84b3bf44cee4639c49b752323b6dcfc1
Mohammad Faraji <m.faraji9841@gmail.com>
2025-03-21 17:37:04 +0330

    IAboutMeRepository Methods

---
+++ OurResumeIR.Domain/Interfaces/IAboutMeRepository.cs
@@ -0,0 +1,17 @@
+﻿using OurResumeIR.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurResumeIR.Domain.Interfaces
+{
+    public interface IAboutMeRepository
+    {
+        Task GetByUserIdAsync(int userId);
+        Task AddAsync(AboutMe aboutMe);
+        Task UpdateAsync(AboutMe aboutMe);
+        Task DeleteAsync(int userId);
+    }
+}
