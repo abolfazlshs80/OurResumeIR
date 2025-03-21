9cb4683c0b5c318309a90cd30dee84cdba66fa94
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-03-21 17:19:41 +0330

    add interface IExpertiseLayerLayerRepository and IUserExpertiseRepository and IExpertiseRepository and IExpertiseRepository

---
+++ OurResumeIR.Domain/Interfaces/IExpertiseRepository.cs
@@ -0,0 +1,20 @@
+﻿using OurResumeIR.Domain.Models;
+using System.Linq.Expressions;
+
+namespace OurResumeIR.Domain.Interfaces;
+
+public interface IExpertiseRepository
+{
+
+    Task<IEnumerable<Expertise>> FindAsync(Expression<Func<Expertise, bool>> predicate);
+    Task<IQueryable<Expertise>> GetAll();
+    Task<IQueryable<Expertise>> GetAllDetails();
+    Task<Expertise> GetById(int Id);
+    Task<Expertise> GetByExpertiseLayerId(int ExpertiseLayerId);
+    Task<Expertise> GetExpertiseByName(string Name);
+
+    Task<Expertise> CreateExpertise(Expertise Expertise);
+    Task<Expertise> UpdateExpertise(Expertise Expertise);
+    Task<Expertise> DeleteExpertise(int ExpertiseId);
+    Task<Expertise> SaveChanges();
+}
