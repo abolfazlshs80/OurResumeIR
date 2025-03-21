d51e8ba7fd03da2ee8f91d23c23b7af0bc7dfc44
Mohammad Faraji <m.faraji9841@gmail.com>
2025-03-21 14:05:48 +0330

    Add Email

--- OurResumeIR.Domain/Models/User.cs
+++ OurResumeIR.Domain/Models/User.cs
@@ -11,5 +11,6 @@ namespace OurResumeIR.Domain.Models
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
