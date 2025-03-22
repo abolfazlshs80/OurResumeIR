f1783ae5feb3aaf4b8a4ca7e652991a468c5c445
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-03-22 10:31:30 +0330

    add UserConfiguration for fluent api

---
+++ OurResumeIR.InfraData/Configuration/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OurResumeIR.Domain.Models;
+
+namespace OurResumeIR.Infra.Data.Configuration
+{
+
+
+ 
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Username).IsRequired().HasMaxLength(100);
+
+            //// تنظیمات فیلد Shadow برای به‌روزرسانی تاریخ
+            //builder.Property<DateTime>("UpdatedDate");
+        }
+    }
+}
