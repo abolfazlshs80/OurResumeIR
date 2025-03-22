a5788e43bc6c12b947a67ec081a5fb5f1303ecbe
Mohammad Faraji <m.faraji9841@gmail.com>
2025-03-22 13:11:41 +0330

    Add Fluent Api DocumentConfiguration

---
+++ OurResumeIR.InfraData/Configuration/DocumentCofigoration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OurResumeIR.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurResumeIR.Infra.Data.Configuration
+{
+    public class DocumentCofigoration : IEntityTypeConfiguration<Documents>
+    {
+        public void Configure(EntityTypeBuilder<Documents> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property( d => d.Name)
+                .IsRequired()
+                .HasMaxLength(256)
+                .HasColumnType("String");
+
+            builder.Property(d => d.ImageName)
+                .IsRequired()
+                .HasMaxLength(256)
+                .HasColumnType ("String");
+
+            builder.HasOne(d => d.User)
+                .WithMany(d => d.Documents);
+        }
+    }
+}
