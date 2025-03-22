5d75253eeb5ea09e67b89f71fb8ed0eb972f0332
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-03-22 18:01:53 +0330

    add fluent api for Abolfazl

---
+++ OurResumeIR.InfraData/Configuration/ExpertiseLayerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OurResumeIR.Domain.Models;
+
+namespace OurResumeIR.Infra.Data.Configuration
+{
+
+
+ 
+    public class ExpertiseLayerConfiguration : IEntityTypeConfiguration<ExpertiseLayer>
+    {
+        public void Configure(EntityTypeBuilder<ExpertiseLayer> builder)
+        {
+            builder.HasKey(e => e.Id);
+        /   builder.Property(e => e.Name).IsRequired().HasMaxLength(100);
+
+            //// تنظیمات فیلد Shadow برای به‌روزرسانی تاریخ
+            //builder.Property<DateTime>("UpdatedDate");
+        }
+    }
+}
