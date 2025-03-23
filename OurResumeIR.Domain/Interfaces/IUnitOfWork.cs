18c973991428d7afcc4ac32b6c58b9daccbebfb8
Mohammad Faraji <m.faraji9841@gmail.com>
2025-03-23 10:18:32 +0330

    Add IUintOfWork To Project

---
+++ OurResumeIR.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurResumeIR.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAboutMeRepository AboutMeRepository { get; }
+        IBlogRepository BlogRepository { get; }
+        IDocumentsRepository DocumentsRepository { get; }
+        IMyExperiencesRepository MyExperiencesRepository { get; }
+        IUserRepository UserRepository { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ OurResumeIR.InfraData/Repositories/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using OurResumeIR.Domain.Interfaces;
+using OurResumeIR.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurResumeIR.Infra.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public IAboutMeRepository AboutMeRepository { get; }
+        public IBlogRepository BlogRepository { get; }
+        public IDocumentsRepository DocumentsRepository { get; }
+        public IMyExperiencesRepository MyExperiencesRepository { get; }
+        public IUserRepository UserRepository { get; }
+
+        public UnitOfWork(
+            AppDbContext context,
+            IAboutMeRepository aboutMeRepository,
+            IBlogRepository blogRepository,
+            IDocumentsRepository documentsRepository,
+            IMyExperiencesRepository myExperiencesRepository,
+            IUserRepository userRepository)
+        {
+            _context = context;
+            AboutMeRepository = aboutMeRepository;
+            BlogRepository = blogRepository;
+            DocumentsRepository = documentsRepository;
+            MyExperiencesRepository = myExperiencesRepository;
+            UserRepository = userRepository;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+
+}
