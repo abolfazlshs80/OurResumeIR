0d09b14a4266d9cb6d8ddf0df0cc40d4d3852fbd
Mohammad Faraji <m.faraji9841@gmail.com>
2025-03-23 12:46:11 +0330

    Add Infara.Ioc And DependencyContainer To Project

---
+++ OurResumeIR.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OurResumeIR.Domain.Interfaces;
+using OurResumeIR.Infra.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurResumeIR.Infra.Ioc
+{
+    public static class DependencyContainer
+    {
+        public static void RegisterService(this IServiceCollection services)
+        {
+            services.AddScoped<IAboutMeRepository, AboutRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IBlogRepository,BlogRepository>();
+            services.AddScoped<IDocumentsRepository, DocumentRepository>();
+            services.AddScoped<IExpertiseLayerRepository, ExpertiseLayerRepository>();
+            services.AddScoped<IMyExperiencesRepository, MyExperienceRepository >();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUserExpertiseRepository, UserExpertiseRepository >();
+        }
+    }
+}
