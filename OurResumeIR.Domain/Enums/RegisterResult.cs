dc1a07c31ddc172f23b31182a9d64529609acd58
Mohammad Faraji <m.faraji9841@gmail.com>
2025-04-06 10:52:03 +0330

    Added Enums RegisterResult

---
+++ OurResumeIR.Domain/Enums/RegisterResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurResumeIR.Domain.Enums
+{
+     public enum RegisterResult
+    {
+        Success = 1,
+        DupplicateEmail = 2,
+        UnequalPassAndRePass = 3,
+
+    }
+}
