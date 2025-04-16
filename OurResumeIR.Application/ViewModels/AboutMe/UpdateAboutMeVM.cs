769ab555675358ed7563bd9c983db03fdaece23a
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-16 20:15:56 +0330

    Add ViewModel AboutMe

---
+++ OurResumeIR.Application/ViewModels/AboutMe/UpdateAboutMeVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurResumeIR.Application.ViewModels.AboutMe
+{
+    public class UpdateAboutMeVM
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+        public string UserId { get; set; }
+    }
+}
