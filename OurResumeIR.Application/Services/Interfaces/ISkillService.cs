a732f0b9c3fc129f3264c9ec3b2b302f51d2867b
Mohammad Faraji <m.faraji9841@gmail.com>
2025-04-23 10:10:08 +0330

    Delete Comment From ISkillService

--- OurResumeIR.Application/Services/Interfaces/ISkillService.cs
+++ OurResumeIR.Application/Services/Interfaces/ISkillService.cs
@@ -25,8 +25,7 @@ namespace OurResumeIR.Application.Services.Interfaces
 
 
         #region Specialties 
-
-        // TO DO Interface
+           
 
         Task<List<SkillFormViewModel>> GetAllAsync();
         Task CreateAsync(SkillFormViewModel model);
@@ -41,19 +40,11 @@ namespace OurResumeIR.Application.Services.Interfaces
 
 
         #region My Skills
-        // ساخت ویو مدل برای نمایش نام تخصص و سطح تخصص داخل کنترولر
-        // نوشتن متد گرفتن نام تخصص و سطح تخصص از ریپوزیتوری و خروجی ویو مدل برای کنترولر
         Task<List<MySkillsForListViewModel>> GetAllSkillAndSkillLevelForViewAsync(string userId);
-        // ساخت ویو مدل برای پر کردن دراپ دان های سطح تخصص و تخصص
-        // نوشتن متد گرفتن نام و سطح تخصص از ریپوزیتوری و خروجی ویو مدل و پر کردن سلکت آپشن ها
         Task<AddMySkillsViewModel> GetAllSkillAndSkillLevelForDropDownAsync();
-        // نوشتن متد  که ورودی ویو مدل دارد و تبدیل اطلاعات ورودی به مدل و صدا زدن متد ریپوزیتوری برای اد کردن مدل در دیتابیس
         Task<bool> AddMySkillAsync(AddMySkillsViewModel model , string userId);
-        // درست کردن ویو مدل برای پر کردن دراپ دان های تخصص و سطح تخصص که در هنگام ویرایش مقدار لازم پر شده باشد
-        // نوشتن یک متد که خروجی ویو مدل داشته باشد و مقدار سلکت آپشن ها پر شود و برای نمایش به کاربر آماده باشد
         EditMySkillsViewModel GetSkillForEditAsync(int userToSkillId ,out  List<SelectListItem> skill , out List<SelectListItem> skillLevel);
         Task FillDropDownsForEditViewModel(EditMySkillsViewModel model);
-        // نوشتن متدی که ورودی ویو مدل دارد و تبدیل ویو مدل به مدل و صدا زدن متد ریپوزیتوری برای ثبت اطلاعات در بانک اطلاعاتی
         Task UpdateUserSkillAsync(EditMySkillsViewModel model);
         Task<bool> DeleteUserSkillAsync(int id, string userId);
         Task FillDropDownsAsync(AddMySkillsViewModel model);
