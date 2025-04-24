26a5f85a74289f460a3c6059adf66e7b4c9ee25f
Mohammad Faraji <m.faraji9841@gmail.com>
2025-04-24 14:42:56 +0330

    Create Action Method AddBlog

--- OurResumeIR.MVC/Areas/User/Controllers/BlogController.cs
+++ OurResumeIR.MVC/Areas/User/Controllers/BlogController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace OurResumeIR.MVC.Areas.User.Controllers
 {
+    [Area("User")]
+    [Authorize]
     public class BlogController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AddBlog()
         {
             return View();
         }
