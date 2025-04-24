5250f8d7e0466d5af22cfa2958c7da89a6c429d7
Mohammad Faraji <m.faraji9841@gmail.com>
2025-04-24 14:04:08 +0330

    Create BlogController

---
+++ OurResumeIR.MVC/Areas/User/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OurResumeIR.MVC.Areas.User.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
