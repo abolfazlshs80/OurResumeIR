17a83f358835810ffee88f4afeab6a844cf1fb14
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-27 11:12:59 +0330

    Update CreateDocumetnModel replace IformFile

--- OurResumeIR.Application/ViewModels/Document/CreateDocumentVM.cs
+++ OurResumeIR.Application/ViewModels/Document/CreateDocumentVM.cs
@@ -1,11 +1,17 @@
-﻿namespace OurResumeIR.Application.ViewModels.Document
+﻿using Microsoft.AspNetCore.Http;
+
+namespace OurResumeIR.Application.ViewModels.Document
 {
     public class CreateDocumentVM
     {
 
         public string Name { get; set; }
-        public string ImageName { get; set; }
-        public string? FileName { get; set; }
+        public IFormFile Image { get; set; }
+   //     public string ImageName { get; set; }
+       // public string? FileName { get; set; }
+        public IFormFile File { get; set; }
         public string UserId { get; set; }
+
+
     }
 }
