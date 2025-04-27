a2b4d3de2f957c5364b5a6751860fa86a615a8fa
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-27 12:01:02 +0330

    update viewmodel update document remove requred

--- OurResumeIR.Application/ViewModels/Document/UpdateDocumentVM.cs
+++ OurResumeIR.Application/ViewModels/Document/UpdateDocumentVM.cs
@@ -9,11 +9,10 @@ namespace OurResumeIR.Application.ViewModels.Document
         [Required(ErrorMessage = "نام را وارد کنید")]
         [Display(Name = "نام مدرک")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "عکس را وارد کنید")]
+        
         [Display(Name = "عکس مدرک")]
         public IFormFile? Image { get; set; }
 
-        [Required(ErrorMessage = "فایل مدرک را وارد  کنید")]
         [Display(Name = "فایل مدرک")]
         public IFormFile? File { get; set; }
         public string UserId { get; set; }
