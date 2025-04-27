8cd21431cf54480192674fc763026d07d52264bd
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-27 11:28:39 +0330

    Add Dataantation

--- OurResumeIR.Application/ViewModels/Document/CreateDocumentVM.cs
+++ OurResumeIR.Application/ViewModels/Document/CreateDocumentVM.cs
@@ -1,14 +1,20 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace OurResumeIR.Application.ViewModels.Document
 {
     public class CreateDocumentVM
     {
-
+        [Required(ErrorMessage = "نام را وارد کنید")]
+        [Display(Name = "نام مدرک")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "عکس را وارد کنید")]
+        [Display(Name = "عکس مدرک")]
         public IFormFile Image { get; set; }
-   //     public string ImageName { get; set; }
-       // public string? FileName { get; set; }
+        //     public string ImageName { get; set; }
+        // public string? FileName { get; set; }
+        [Required(ErrorMessage = "فایل مدرک را وارد  کنید")]
+        [Display(Name = "فایل مدرک")]
         public IFormFile File { get; set; }
         public string UserId { get; set; }
 
