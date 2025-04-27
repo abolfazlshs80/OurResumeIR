931ce41dfcf0b69d776d074a35cc9806c2d02f9c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-27 11:35:26 +0330

    update viewmode update document

--- OurResumeIR.Application/ViewModels/Document/UpdateDocumentVM.cs
+++ OurResumeIR.Application/ViewModels/Document/UpdateDocumentVM.cs
@@ -1,13 +1,24 @@
-﻿namespace OurResumeIR.Application.ViewModels.Document
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace OurResumeIR.Application.ViewModels.Document
 {
     public class UpdateDocumentVM
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "نام را وارد کنید")]
+        [Display(Name = "نام مدرک")]
         public string Name { get; set; }
-        public string ImageName { get; set; }
-        public string? FileName { get; set; }
+        [Required(ErrorMessage = "عکس را وارد کنید")]
+        [Display(Name = "عکس مدرک")]
+        public IFormFile Image { get; set; }
+        //     public string ImageName { get; set; }
+        // public string? FileName { get; set; }
+        [Required(ErrorMessage = "فایل مدرک را وارد  کنید")]
+        [Display(Name = "فایل مدرک")]
+        public IFormFile File { get; set; }
         public string UserId { get; set; }
-        //public IFormFile File { get; set; }
+       
 
     }
 }
