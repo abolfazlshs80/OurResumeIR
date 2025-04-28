fe7970b9a58e50fa0f2a7c17dce286b454d314d7
Mohammad Faraji <m.faraji9841@gmail.com>
2025-04-28 10:07:03 +0330

    Create ManageHistoryController Controller And Added Action Method  AddHistory

---
+++ OurResumeIR.MVC/Areas/User/Controllers/ManageHistoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OurResumeIR.MVC.Areas.User.Controllers
+{
+    public class ManageHistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AddHistory()
+        {
+            return View();
+        }
+    }
+}
