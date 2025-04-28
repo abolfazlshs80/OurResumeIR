5bd59026fbbf16f1680adbe400ebb38d68cdaeb5
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-28 10:16:43 +0330

    Add About Component

--- OurResumeIR.MVC/ViewComponents/Handler/Home/AboutHomeViewComponent.cs
+++ OurResumeIR.MVC/ViewComponents/Handler/Home/AboutHomeViewComponent.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OurResumeIR.Application.Services.Interfaces;
+using OurResumeIR.Application.ViewModels.AboutMe;
 using OurResumeIR.Application.ViewModels.Account;
 
 namespace OurResumeIR.MVC.ViewComponents.Handler.Home
 {
-    public class HeaderViewComponent
+    public class AboutHomeViewComponent
         (IUserService _userService)
         : ViewComponent
     {
 
-        public async Task<IViewComponentResult> InvokeAsync(UserResumeVM model)
+        public async Task<IViewComponentResult> InvokeAsync(AboutMeVM model)
         {
 
-            return View("/ViewComponents/Views/Home/Header.cshtml", model);
+            return View("/ViewComponents/Views/Home/about.cshtml", model);
         }
 
     }
---
+++ OurResumeIR.MVC/ViewComponents/Handler/Home/HeaderHomeViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OurResumeIR.Application.Services.Interfaces;
+using OurResumeIR.Application.ViewModels.Account;
+
+namespace OurResumeIR.MVC.ViewComponents.Handler.Home
+{
+    public class HeaderHomeViewComponent
+        (IUserService _userService)
+        : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(UserResumeVM model)
+        {
+
+            return View("/ViewComponents/Views/Home/Header.cshtml", model);
+        }
+
+    }
+}
