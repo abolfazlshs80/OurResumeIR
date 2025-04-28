077a5fc496ebb9349fdb9ab5256cbecac79011f1
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-28 09:25:27 +0330

    update database for user

--- OurResumeIR.InfraData/Migrations/20250428055331_add-link-bio-fileresume-table-user.Designer.cs
+++ OurResumeIR.InfraData/Migrations/20250428055331_add-link-bio-fileresume-table-user.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OurResumeIR.Infra.Data.Context;
 
@@ -11,9 +12,11 @@ using OurResumeIR.Infra.Data.Context;
 namespace OurResumeIR.Infra.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250428055331_add-link-bio-fileresume-table-user")]
+    partial class addlinkbiofileresumetableuser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OurResumeIR.InfraData/Migrations/20250428055331_add-link-bio-fileresume-table-user.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OurResumeIR.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addlinkbiofileresumetableuser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LinkInstagram",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LinkLinkdin",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LinkTelegram",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LinkX",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResumeFile",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "bio",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LinkInstagram",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LinkLinkdin",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LinkTelegram",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LinkX",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ResumeFile",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "bio",
+                table: "AspNetUsers");
+        }
+    }
+}
