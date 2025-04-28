49b9d0f991856c8003560d10a1294d3dc5993e6d
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-28 09:26:19 +0330

    Add Component Header

---
+++ OurResumeIR.MVC/ViewComponents/Handler/Home/HeaderViewComponent.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing.Printing;
+using OurResumeIR.Application.Services.Interfaces;
+using OurResumeIR.Infra.Data.Context;
+using System.Security.Claims;
+
+namespace SlideCloud.ViewComponents
+{
+    public class HeaderViewComponent
+        (IUserService _userService)
+        : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(string userId)
+        {
+
+            var model =await _userService.LoadProfile(userId);
+            return View("/ViewComponents/Views/Home/Header.cshtml", model);
+        }
+
+    }
+}
