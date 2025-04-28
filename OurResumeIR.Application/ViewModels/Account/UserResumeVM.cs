22d6dc589c228aefe3506f671e2dc4c69eb46399
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-28 09:24:39 +0330

    Add Model for load resume

---
+++ OurResumeIR.Application/ViewModels/Account/UserResumeVM.cs
@@ -0,0 +1,26 @@
+﻿using OurResumeIR.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OurResumeIR.Application.ViewModels.AboutMe;
+using OurResumeIR.Application.ViewModels.Document;
+using OurResumeIR.Application.ViewModels.Blog;
+using OurResumeIR.Application.ViewModels.MySkills;
+
+namespace OurResumeIR.Application.ViewModels.Account
+{
+    public class UserResumeVM
+    {
+        public string? Slug { get; set; }
+        public string? FullName { get; set; }
+        public string? ImageName { get; set; }
+        // Navigation Property  
+        public ICollection<MySkillsForListViewModel> MySkill { get; set; }
+        public ICollection<BlogPostListViewModel> Blog { get; set; }
+        public ICollection<DocumentVM> Documents { get; set; }
+        public ICollection<History> History { get; set; }
+        public AboutMeVM AboutMe { get; set; }
+    }
+}
