86cb2a9d31044f55fa875863ef3f61b4c17f6556
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-05 20:24:00 +0330

    create viewmodel contactus

---
+++ OurResumeIR.Application/ViewModels/ContactUs/AdminItemContactUsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurResumeIR.Application.ViewModels.ContactUs
+{
+    public class AdminItemContactUsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Text { get; set; }
+        public string UserId { get; set; }
+    }
+}
