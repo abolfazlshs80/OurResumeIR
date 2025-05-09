33ce0d4bc6533cdc3122e4f66acd4f04db5b3798
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-09 17:26:40 +0330

    add seed data

--- OurResumeIR.Infrastructure/Configuration/UserConfiguration.cs
+++ OurResumeIR.Infrastructure/Configuration/UserConfiguration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using OurResumeIR.Domain.Models;
@@ -11,11 +12,13 @@ namespace OurResumeIR.Infra.Data.Configuration
 {
 
 
- 
+
     public class UserConfiguration : IEntityTypeConfiguration<User>
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
+            var userId = Guid.NewGuid().ToString();
+            var hasher = new PasswordHasher<User>();
             builder.HasKey(e => e.Id);
             //builder.Property(e => e.Username).IsRequired().HasMaxLength(100);
 
@@ -38,7 +41,24 @@ namespace OurResumeIR.Infra.Data.Configuration
                 .HasForeignKey(a => a.UserId);
             builder.HasOne(a => a.AboutMe)
                 .WithOne(a => a.User)
-                .HasForeignKey< AboutMe>(a => a.UserId);
+                .HasForeignKey<AboutMe>(a => a.UserId);
+
+    
+
+            var adminUser = new User
+            {
+                Id = userId,
+                UserName = "admin@example.com",
+                NormalizedUserName = "ADMIN@EXAMPLE.COM",
+                Email = "admin@example.com",
+                NormalizedEmail = "ADMIN@EXAMPLE.COM",
+                EmailConfirmed = true,
+                FullName = "Admin User",
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            adminUser.PasswordHash = hasher.HashPassword(adminUser, "Admin123!");
+            builder.HasData(adminUser);
             //// تنظیمات فیلد Shadow برای به‌روزرسانی تاریخ
             //builder.Property<DateTime>("UpdatedDate");
         }
