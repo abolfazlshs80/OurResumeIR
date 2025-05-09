6905c60268517353edadd12ff66deba2a1d40b28
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-09 16:41:25 +0330

    refactor blog service

---
+++ OurResumeIR.Application/Static/FolderNameExtentions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace OurResumeIR.Application.Static
+{
+    public static class FolderNameExtentions
+    {
+        public const string Blog = "Blog";
+ 
+
+    }
+}
