6a859f0806c94c5fbff0657be427fc6d3673ca2b
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-09 17:00:05 +0330

    update skill service

---
+++ OurResumeIR.Application/Services/Implementation/SkillLevelService.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OurResumeIR.Application.ViewModels.Experience;
+using OurResumeIR.Application.ViewModels.ExpertiseLayers;
+using OurResumeIR.Application.ViewModels.MySkills;
+using OurResumeIR.Domain.Interfaces;
+using OurResumeIR.Domain.Models;
+
+namespace OurResumeIR.Application.Services.Interfaces;
+
+public class SkillLevelService(IUnitOfWork unitOfWork, IMapper mapper) : ISkillLevelService
+{
+
+    public async Task<List<SkillLevelVM>> GetAll()
+    {
+        var rep_SkillLevel = unitOfWork.SkillLevelRepository;
+        try
+        {
+            var list = (await rep_SkillLevel.FindAsync(a => a.Id != 0)).ToList();
+            return mapper.Map<List<SkillLevelVM>>(list);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+    }
+
+    public async Task<SkillLevelVM> GetById(int Id)
+    {
+        var rep_SkillLevel = unitOfWork.SkillLevelRepository;
+        return mapper.Map<SkillLevelVM>((await rep_SkillLevel.FindAsync(a => a.Id == Id)).FirstOrDefault());
+    }
+
+    public async Task<bool> Update(UpdateSkillLevelVM model)
+    {
+        var rep_SkillLevel = unitOfWork.SkillLevelRepository;
+        var newModel = new SkillLevel()
+        {
+            Id = model.Id,
+            Name = model.Name,
+            Percentage = model.Percentage
+        };
+        bool status = await rep_SkillLevel.UpdateSkillLevelLevel(newModel);
+        await rep_SkillLevel.SaveChanges();
+
+        return status;
+    }
+
+    public async Task<bool> Create(CreateSkillLevelVM model)
+    {
+        var rep_SkillLevel = unitOfWork.SkillLevelRepository;
+        var newModel = mapper.Map<SkillLevel>(model);
+        int id = await rep_SkillLevel.CreateSkillLevelLevel(newModel);
+        await rep_SkillLevel.SaveChanges();
+        if (newModel.Id != 0)
+            return true;
+
+        return false;
+
+    }
+
+    public async Task<bool> Delete(int Id)
+    {
+        var rep_SkillLevel = unitOfWork.SkillLevelRepository;
+        var status = await rep_SkillLevel.DeleteSkillLevelLevel(Id);
+        await rep_SkillLevel.SaveChanges();
+        return status;
+
+    }
+
+
+
+
+
+
+
+
+}
--- OurResumeIR.Application/Services/Interfaces/ISkillService.cs
+++ OurResumeIR.Application/Services/Interfaces/ISkillService.cs
@@ -13,20 +13,7 @@ namespace OurResumeIR.Application.Services.Interfaces
 {
     public interface ISkillService
     {
-        #region Specialties Layer
-
-        Task<List<SkillLevelVM>> GetAll();
-        Task<SkillLevelVM> GetById(int Id);
-        Task<bool> Update(UpdateSkillLevelVM model);
-        Task<bool> Create(CreateSkillLevelVM model);
-        Task<bool> Delete(int Id);
-
-        #endregion
-
-
-        #region Specialties 
-
-        // TO DO Interface
+     
 
         Task<List<SkillFormViewModel>> GetAllAsync();
         Task CreateAsync(SkillFormViewModel model);
@@ -37,9 +24,24 @@ namespace OurResumeIR.Application.Services.Interfaces
         Task<bool> UpdateSkillAsync(SkillFormViewModel model);
         Task<bool> DeleteSkillAsync(int id);
 
-        #endregion
+   
 
 
+        
+
+    }
+
+    public interface ISkillLevelService
+    {
+        Task<List<SkillLevelVM>> GetAll();
+        Task<SkillLevelVM> GetById(int Id);
+        Task<bool> Update(UpdateSkillLevelVM model);
+        Task<bool> Create(CreateSkillLevelVM model);
+        Task<bool> Delete(int Id);
+    }
+
+    public interface IMySkillService
+    {
         #region My Skills
         // ساخت ویو مدل برای نمایش نام تخصص و سطح تخصص داخل کنترولر
         // نوشتن متد گرفتن نام تخصص و سطح تخصص از ریپوزیتوری و خروجی ویو مدل برای کنترولر
@@ -51,12 +53,11 @@ namespace OurResumeIR.Application.Services.Interfaces
         Task AddMySkillAsync(AddMySkillsViewModel model);
         // درست کردن ویو مدل برای پر کردن دراپ دان های تخصص و سطح تخصص که در هنگام ویرایش مقدار لازم پر شده باشد
         // نوشتن یک متد که خروجی ویو مدل داشته باشد و مقدار سلکت آپشن ها پر شود و برای نمایش به کاربر آماده باشد
-        EditMySkillsViewModel GetSkillForEditAsync(int userToSkillId ,out  List<SelectListItem> skill , out List<SelectListItem> skillLevel);
+        EditMySkillsViewModel GetSkillForEditAsync(int userToSkillId, out List<SelectListItem> skill, out List<SelectListItem> skillLevel);
         Task FillDropDownsForEditViewModel(EditMySkillsViewModel model);
         // نوشتن متدی که ورودی ویو مدل دارد و تبدیل ویو مدل به مدل و صدا زدن متد ریپوزیتوری برای ثبت اطلاعات در بانک اطلاعاتی
         Task UpdateUserSkillAsync(EditMySkillsViewModel model);
         Task<bool> DeleteUserSkillAsync(int id, string userId);
         #endregion
-
     }
 }
